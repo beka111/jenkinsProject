912ec62bbaff12efd545e90f211a36fccd4ff024
Kubanychbek Tumonbaev <Kuba@Kubanychbeks-MacBook-Pro-2.local>
2019-07-15 20:30:35 -0500

    Initial commit

---
+++ src/test/java/jenkinsProject.java
@@ -0,0 +1,9 @@
+public class jenkinsProject {
+    public static void main(String[] args) {
+
+        String str="I love jenkins";
+        for (int i=0;i<10;i++){
+            System.out.println(str);
+        }
+    }
+}
